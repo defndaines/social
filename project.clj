c32adaf00eac44f6e3d2f851d3126e9af38021c0
defndaines <defndaines@gmail.com>
2016-12-29 08:46:12 -0800

    Updates all versions to latest
    Unable to run service with old versions, so checking if version updates
    help. Scratch commands do work on the service now, but wiring isn't
    quite right.

--- project.clj
+++ project.clj
@@ -1,23 +1,22 @@
-(defproject social "0.0.1-SNAPSHOT"
+(defproject social "0.0.2-SNAPSHOT"
   :description "A social app kata."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/defndaines/social"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
-                 [io.pedestal/pedestal.service "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [io.pedestal/pedestal.service "0.5.1"]
 
-                 [io.pedestal/pedestal.jetty "0.3.1"]
+                 [io.pedestal/pedestal.jetty "0.5.1"]
 
-                 [ch.qos.logback/logback-classic "1.1.2" :exclusions [org.slf4j/slf4j-api]]
-                 [org.slf4j/jul-to-slf4j "1.7.7"]
-                 [org.slf4j/jcl-over-slf4j "1.7.7"]
-                 [org.slf4j/log4j-over-slf4j "1.7.7"]
-                 [clj-http "1.0.1"]
-                 [clj-time "0.9.0"]]
-  :dev-dependencies [[ring/ring-mock "0.2.0"]]
+                 [ch.qos.logback/logback-classic "1.1.8" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.22"]
+                 [org.slf4j/jcl-over-slf4j "1.7.22"]
+                 [org.slf4j/log4j-over-slf4j "1.7.22"]
+                 [clj-http "3.4.1"]
+                 [clj-time "0.13.0"]]
+  :dev-dependencies [[ring/ring-mock "0.3.0"]]
   :min-lein-version "2.0.0"
   :resource-paths ["config", "resources"]
   :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "social.server/run-dev"]}
-                   :dependencies [[io.pedestal/pedestal.service-tools "0.3.1"]]}}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.1"]]}}
   :main ^{:skip-aot true} social.server)
-
